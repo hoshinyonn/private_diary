711e631c7930149cf637596c5f7ffab718d0d081
hoshinyonn <ngn2149610@stu.o-hara.ac.jp>
2022-07-22 10:44:12 +0900

    第12章まで

---
+++ private_diary/settings_dev.py
@@ -0,0 +1,67 @@
+from.settings_common import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-x2w)f#40u_&_fj^(u@$_q%a-_i(9blbgy)aj_-+(qi=6_@c$uz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# ロギング設定
+LOGGING = {
+    'version': 1,  # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
+
+# コンソール用
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Gmail用
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Gmailのメールサーバーへの接続設定
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = '0ha022hno@gmail.com'
+#EMAIL_HOST_PASSWORD = 'pgxnlykctrvaxilh'
+#EMAIL_USE_TLS = True
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
